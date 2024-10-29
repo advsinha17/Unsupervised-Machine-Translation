074a4d4c14288e518df86c264565609c81d1c976
Advik Sinha <advsinha112@gmail.com>
2024-10-29 16:33:06 +0530

    feat: add data collate function

--- src/utils/dataloader.py
+++ src/utils/dataloader.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
+import torch 
 
 class UNMTDataset(Dataset):
     def __init__(self, tokenizer: BertTokenizer, lang: str, size: int = 10000):
@@ -17,11 +18,20 @@ class UNMTDataset(Dataset):
     
     def _tokenize_line(self, line):
         split_line = line.split()
-        tokenized_line = self.tokenizer(split_line, is_split_into_words = True, return_tensors = 'pt')
+        tokenized_line = self.tokenizer(split_line, is_split_into_words = True)
         word_idx = tokenized_line.word_ids()
         return tokenized_line, word_idx
 
     def __getitem__(self, idx):
         line = self.data[idx]
         tokenized_line, word_idx = self._tokenize_line(line['text'])
-        return tokenized_line, word_idx
+        input_ids = tokenized_line['input_ids']
+        attention_mask = tokenized_line['attention_mask']
+        return input_ids, attention_mask, word_idx
+    
+
+def data_collate(batch, tokenizer):
+    input_ids = torch.nn.utils.rnn.pad_sequence([item[0] for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
+    attention_mask = torch.nn.utils.rnn.pad_sequence([item[1] for item in batch], batch_first=True, padding_value=0)
+    word_idx = [item[2] for item in batch]
+    return input_ids, attention_mask, word_idx
