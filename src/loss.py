68a0799bf88eb688c4228343adfbd55ce166334f
Advik Sinha <advsinha112@gmail.com>
2024-11-02 18:58:54 +0530

    feat: add reconstruction loss

--- src/loss.py
+++ src/loss.py
@@ -1,5 +1,18 @@
 import torch
 from torch import nn
+from utils.decodertokens import UNMTDecoderTokens
+
+class ReconstructionLoss:
+    def __init__(self, DecoderTokens: UNMTDecoderTokens):
+        self.loss = nn.CrossEntropyLoss()
+        
+    def calculate_loss(self, dec_out: torch.Tensor, target: torch.Tensor):
+        # dec_out -> [batch_size, seq_len, vocab_size], [[[0.xx, probabilities...]]]
+        # target -> [batch_size, seq_len, index in tokenizer vocab], eg [[[101, 56824, 485, ...]]] 
+        # so need to map each index in dec_out to correspoding index of tokenizer vocab & vice versa
+        # so then target can be -> [[[1, 5, 3, 8 ...]]]
+
+        return self.loss(dec_out, target)
 
 class UNMTLoss:
     def __init__(self, src_vocab, tgt_vocab): # TODO: type-hints for src and tgt_vocab
