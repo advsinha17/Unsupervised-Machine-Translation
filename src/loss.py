df04c2b8dd0aef1cd570c07c743dc40de7a5de8a
Advik Sinha <advsinha112@gmail.com>
2024-11-02 19:10:11 +0530

    feat: complete reconstruction loss

--- src/loss.py
+++ src/loss.py
@@ -5,13 +5,24 @@ from utils.decodertokens import UNMTDecoderTokens
 class ReconstructionLoss:
     def __init__(self, DecoderTokens: UNMTDecoderTokens):
         self.loss = nn.CrossEntropyLoss()
+        self.decoder_tokens = DecoderTokens
+
+    def map_target(self, target: torch.Tensor):
+        mapped_target = target.clone()
+        for i in range(target.size(0)):
+            for j in range(target.size(1)):
+                mapped_target[i, j] = self.decoder_tokens.tokenizer_to_id.get(target[i, j].item(), -1)
+        
+        return mapped_target
         
     def calculate_loss(self, dec_out: torch.Tensor, target: torch.Tensor):
-        # dec_out -> [batch_size, seq_len, vocab_size], [[[0.xx, probabilities...]]]
-        # target -> [batch_size, seq_len, index in tokenizer vocab], eg [[[101, 56824, 485, ...]]] 
+        # dec_out -> [batch_size, seq_len, vocab_size], [[[0.xx, probabilities...]]] -> 3d
+        # target -> [batch_size, seq_len, index in tokenizer vocab], eg [[[101, 56824, 485, ...]]] -> 2d
         # so need to map each index in dec_out to correspoding index of tokenizer vocab & vice versa
         # so then target can be -> [[[1, 5, 3, 8 ...]]]
-
+        target = self.map_target(target)
+        dec_out = dec_out.view(-1, dec_out.size(-1))
+        target = target.view(-1)
         return self.loss(dec_out, target)
 
 class UNMTLoss:
