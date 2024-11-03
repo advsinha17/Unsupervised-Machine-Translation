892c752022a178ee4626b62164f22bf37673a84e
Advik Sinha <advsinha112@gmail.com>
2024-11-03 13:39:12 +0530

    feat: ignore padding for loss calculation

--- src/loss.py
+++ src/loss.py
@@ -15,14 +15,17 @@ class ReconstructionLoss:
         
         return mapped_target
         
-    def calculate_loss(self, dec_out: torch.Tensor, target: torch.Tensor):
+    def calculate_loss(self, dec_out: torch.Tensor, target: torch.Tensor, attn_mask: torch.Tensor):
         # dec_out -> [batch_size, seq_len, vocab_size], [[[0.xx, probabilities...]]] -> 3d
-        # target -> [batch_size, seq_len, index in tokenizer vocab], eg [[[101, 56824, 485, ...]]] -> 2d
+        # target -> [batch_size, seq_len] of index in tokenizer vocab, eg [[101, 56824, 485, ...]]-> 2d
         # so need to map each index in dec_out to correspoding index of tokenizer vocab & vice versa
-        # so then target can be -> [[[1, 5, 3, 8 ...]]]
+        # so then target can be -> [[1, 5, 3, 8 ...]]
+
+        # currently assuming with teacher forcing, pred is of same length as target
+        mask = attn_mask.view(-1) == 1
         target = self.map_target(target)
         dec_out = dec_out.view(-1, dec_out.size(-1))
-        target = target.view(-1)
+        target = torch.where(mask, target.view(-1), torch.tensor(self.loss.ignore_index).type_as(target))
         return self.loss(dec_out, target)
 
 class UNMTLoss:
