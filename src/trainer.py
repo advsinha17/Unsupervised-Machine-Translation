f27f4f572484464a975f6c2ccd3d9b916fa20674
Advik Sinha <advsinha112@gmail.com>
2024-11-09 18:21:56 +0530

    fix: fix imports

--- src/trainer.py
+++ src/trainer.py
@@ -1,12 +1,12 @@
 from torch.utils.data import DataLoader
 import torch
 from torch import nn
-from loss import ReconstructionLoss
-from utils.decodertokens import UNMTDecoderTokens
+from .loss import ReconstructionLoss
+from .utils.decodertokens import UNMTDecoderTokens
 from transformers import BertTokenizerFast
 from itertools import zip_longest
 from tqdm import tqdm
-from models.models import SEQ2SEQ
+from .models.models import SEQ2SEQ
 
 class Trainer:
     def __init__(self, lang1: DataLoader, lang2: DataLoader, lang3: DataLoader, tokenizer: BertTokenizerFast, lr: float = 0.0003): 
