fb7ce10030c5685ae9c877f8f80e9577f5374a70
Advik Sinha <advsinha112@gmail.com>
2024-11-10 12:11:06 +0530

    feat: finish backtranslation

--- src/trainer.py
+++ src/trainer.py
@@ -14,10 +14,14 @@ class Trainer:
         self.lang2 = lang2
         self.lang3 = lang3
 
-        self.decoder_tokens1 = UNMTDecoderTokens(lang1.tokenizer, lang1.lang)
-        self.decoder_tokens2 = UNMTDecoderTokens(lang2.tokenizer, lang2.lang)
-        self.decoder_tokens3 = UNMTDecoderTokens(lang3.tokenizer, lang3.lang)
-
+        self.decoder_tokens1 = UNMTDecoderTokens(lang1.dataset.tokenizer, lang1.dataset.lang)
+        self.decoder_tokens2 = UNMTDecoderTokens(lang2.dataset.tokenizer, lang2.dataset.lang)
+        self.decoder_tokens3 = UNMTDecoderTokens(lang3.dataset.tokenizer, lang3.dataset.lang)
+        
+        self.decoder_tokens1.load_token_list()
+        self.decoder_tokens2.load_token_list()
+        self.decoder_tokens3.load_token_list()
+        
         self.reconstruction_loss1 = ReconstructionLoss(self.decoder_tokens1)
         self.reconstruction_loss2 = ReconstructionLoss(self.decoder_tokens2)
         self.reconstruction_loss3 = ReconstructionLoss(self.decoder_tokens3)
@@ -28,6 +32,13 @@ class Trainer:
         self.lr = lr
         self.model = None
         self.optimizer = None
+
+    def map_argmax_to_tokenizer_id(self, argmax_tensor, decoder_tokens):
+        mapped_tensor = argmax_tensor.clone()
+        for i in range(argmax_tensor.size(0)):
+            for j in range(argmax_tensor.size(1)):
+                mapped_tensor[i, j] = decoder_tokens.id_to_tokenizer.get(argmax_tensor[i, j].item(), decoder_tokens.tokenizer.pad_token_id)
+        return mapped_tensor
 
     def train(self, model: SEQ2SEQ, epochs: int): 
         self.optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr = self.lr, betas = (0.5, 0.999))
@@ -46,44 +57,52 @@ class Trainer:
                         input_ids1 = input_ids1.to(self.device)
                         attention_mask1 = attention_mask1.to(self.device)
                         noisy_input_ids1 = noisy_input_ids1.to(self.device)
-                        output_ids1 = self.model(0, input_ids1, None, noisy_input_ids1, None) # check out the 2 nones
+                        noisy_attention_mask1 = noisy_attention_mask1.to(self.device)
+                        output_ids1 = self.model(0, input_ids1, attention_mask1, noisy_input_ids1, noisy_attention_mask1, True, True) 
                         loss1 = self.reconstruction_loss1.calculate_loss(output_ids1, input_ids1, attention_mask1)
                         loss1.backward()
                         reconstruction_loss += loss1.item()
 
                         # back translation (0 -> 1 -> 0)
                         with torch.no_grad():
-                            bt_out_0_1 = self.model(1, input_ids1, attention_mask1, None, None) 
+                            bt_out_0_1 = self.model(1, input_ids1, attention_mask1, None, None, False, False) 
+                            bt_out_0_1 = torch.argmax(bt_out_0_1, dim=-1)
+                            bt_out_0_1 = self.map_argmax_to_tokenizer_id(bt_out_0_1, self.decoder_tokens2)
                         attn_mask_0_1_bt = (bt_out_0_1 != self.tokenizer.pad_token_id).to(self.device)
-                        output_ids_0_1_bt = self.model(0, bt_out_0_1, attn_mask_0_1_bt, None, None)
+                        output_ids_0_1_bt = self.model(0, input_ids1, attention_mask1, bt_out_0_1, attn_mask_0_1_bt, True, True)
                         loss_0_1_bt = self.reconstruction_loss1.calculate_loss(output_ids_0_1_bt, input_ids1, attention_mask1)
                         loss_0_1_bt.backward()
                         backtranslation_loss += loss_0_1_bt.item()
 
                         # back translation (0 -> 2 -> 0)    
                         with torch.no_grad():
-                            output_ids1_2 = self.model(2, input_ids1, attention_mask1, None, None) 
-                        attn_mask1_2_bt = (output_ids1_2 != self.tokenizer.pad_token_id).to(self.device)    
-                        output_ids1_2_bt = self.model(0, output_ids1_2, attn_mask1_2_bt, None, None)
-                        loss1_2_bt = self.reconstruction_loss1.calculate_loss(output_ids1_2_bt, input_ids1, attention_mask1)
-                        loss1_2_bt.backward()
-                        backtranslation_loss += loss1_2_bt.item()
+                            bt_out_0_2 = self.model(2, input_ids1, attention_mask1, None, None, False, False)
+                            bt_out_0_2 = torch.argmax(bt_out_0_2, dim=-1) 
+                            bt_out_0_2 = self.map_argmax_to_tokenizer_id(bt_out_0_2, self.decoder_tokens3)
+                        attn_mask_0_2_bt = (bt_out_0_2 != self.tokenizer.pad_token_id).to(self.device)    
+                        output_ids_0_2_bt = self.model(0, input_ids1, attention_mask1, bt_out_0_2, attn_mask_0_2_bt, True, True)
+                        loss_0_2_bt = self.reconstruction_loss1.calculate_loss(output_ids_0_2_bt, input_ids1, attention_mask1)
+                        loss_0_2_bt.backward()
+                        backtranslation_loss += loss_0_2_bt.item()
 
                     if batch2 is not None:
                         input_ids2, attention_mask2, noisy_input_ids2, noisy_attention_mask2, _, _ = batch2
                         input_ids2 = input_ids2.to(self.device)
                         attention_mask2 = attention_mask2.to(self.device)
                         noisy_input_ids2 = noisy_input_ids2.to(self.device)
-                        _, output_ids2, _ = self.model(noisy_input_ids2, attention_mask2)
+                        noisy_attention_mask2 = noisy_attention_mask2.to(self.device)
+                        output_ids2 = self.model(1, input_ids2, attention_mask2, noisy_input_ids2, noisy_attention_mask2, True, True) 
                         loss2 = self.reconstruction_loss2.calculate_loss(output_ids2, input_ids2, attention_mask2)
                         loss2.backward()
                         reconstruction_loss += loss2.item()
 
                         # back translation (1 -> 0 -> 1)
                         with torch.no_grad():
-                            bt_out_1_0 = self.model(0, input_ids1, attention_mask1, None, None) 
+                            bt_out_1_0 = self.model(0, input_ids2, attention_mask2, None, None, False, False) 
+                            bt_out_1_0 = torch.argmax(bt_out_1_0, dim=-1)
+                            bt_out_1_0 = self.map_argmax_to_tokenizer_id(bt_out_1_0, self.decoder_tokens1)
                         attn_mask_1_0 = (bt_out_1_0 != self.tokenizer.pad_token_id).to(self.device)
-                        output_ids_1_0_bt = self.model(1, bt_out_1_0, attn_mask_1_0, None, None)
+                        output_ids_1_0_bt = self.model(1, input_ids2, attention_mask2, bt_out_1_0, attn_mask_1_0, True, True)
                         loss_1_0_bt = self.reconstruction_loss2.calculate_loss(output_ids_1_0_bt, input_ids2, attention_mask2) 
                         loss_1_0_bt.backward()
                         backtranslation_loss += loss_1_0_bt.item()  
@@ -93,14 +112,17 @@ class Trainer:
                         input_ids3 = input_ids3.to(self.device)
                         attention_mask3 = attention_mask3.to(self.device)
                         noisy_input_ids3 = noisy_input_ids3.to(self.device)
-                        _, _, output_ids3 = self.model(noisy_input_ids3, attention_mask3)
+                        noisy_attention_mask3 = noisy_attention_mask3.to(self.device)
+                        output_ids3 = self.model(2, input_ids3, attention_mask3, noisy_input_ids3, noisy_attention_mask3, True, True) 
                         loss3 = self.reconstruction_loss3.calculate_loss(output_ids3, input_ids3, attention_mask3)
                         loss3.backward()
                         reconstruction_loss += loss3.item()
                         with torch.no_grad():
-                            bt_out_2_0 = self.model(0, input_ids1, attention_mask1, None, None) # maybe replace None with noisy input mask
+                            bt_out_2_0 = self.model(0, input_ids3, attention_mask3, None, None, False, False)
+                            bt_out_2_0 = torch.argmax(bt_out_2_0, dim=-1) 
+                            bt_out_2_0 = self.map_argmax_to_tokenizer_id(bt_out_2_0, self.decoder_tokens1)
                         attn_mask_2_0 = (bt_out_2_0 != self.tokenizer.pad_token_id).to(self.device)
-                        output_ids_2_0_bt = self.model(1, bt_out_2_0, attn_mask_2_0, None, None)
+                        output_ids_2_0_bt = self.model(2, input_ids3, attention_mask3, bt_out_2_0, attn_mask_2_0, True, True)
                         loss_2_0_bt = self.reconstruction_loss3.calculate_loss(output_ids_2_0_bt, input_ids3, attention_mask3) 
                         loss_2_0_bt.backward()
                         backtranslation_loss += loss_2_0_bt.item() 
