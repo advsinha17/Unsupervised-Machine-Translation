fa9b75f055a509a6874ee796089f9daa8e89cb1e
Advik Sinha <advsinha112@gmail.com>
2024-11-17 17:25:16 +0530

    fix: fix decodertokens mapping

--- src/utils/decodertokens.py
+++ src/utils/decodertokens.py
@@ -13,16 +13,16 @@ class UNMTDecoderTokens:
         self.tokenizer = tokenizer
         self.lang = lang
         self.token_set = set()
-        self.subfolder_name = "decoder_tokens"
+        self.subfolder_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "decoder_tokens")
+
+        # self.subfolder_name = "./decoder_tokens"
         self.max_examples = max_examples
         self.id_to_tokenizer = {} # maps local id to tokenizer id
         self.tokenizer_to_id = {} # maps tokenizer id to local id 
-        self.id_to_tokenizer[self.tokenizer.pad_token_id] = self.tokenizer.pad_token_id
-        self.tokenizer_to_id[self.tokenizer.pad_token_id] = self.tokenizer.pad_token_id
 
     def _process_data(self):
 
-        current_id = 1
+        # current_id = 1
         with tqdm(desc=f"Processing train", unit=" examples", ncols=80) as pbar:
             for example in self.dataset:
                 if pbar.n >= self.max_examples:
@@ -30,12 +30,11 @@ class UNMTDecoderTokens:
                 sentence = example
                 tokenized_line = self.tokenizer(sentence)
                 self.token_set.update(tokenized_line['input_ids'])
-                for input_id in tokenized_line['input_ids']:
-                    if input_id not in self.tokenizer_to_id:
-                        self.tokenizer_to_id[input_id] = current_id
-                        self.id_to_tokenizer[current_id] = input_id
-                        current_id += 1
                 pbar.update(1)
+        self.token_set = list(sorted(self.token_set))
+        for i, token in enumerate(self.token_set):
+            self.tokenizer_to_id[token] = i
+            self.id_to_tokenizer[i] = token
         
         
     def create_token_list(self):
@@ -43,11 +42,11 @@ class UNMTDecoderTokens:
         if not os.path.exists(self.subfolder_name):
             os.makedirs(self.subfolder_name)
         
-        file_path = os.path.join(os.path.dirname(__file__),self.subfolder_name, f"{self.lang}_decoder_tokens_list.pkl")
+        file_path = os.path.join(self.subfolder_name, f"{self.lang}_decoder_tokens_list.pkl")
 
         with open(file_path, 'wb') as f:
             data = {
-                'token_set': sorted(self.token_set),
+                'token_set': self.token_set,
                 'id_to_tokenizer': self.id_to_tokenizer,
                 'tokenizer_to_id': self.tokenizer_to_id
             }
@@ -57,7 +56,7 @@ class UNMTDecoderTokens:
     
     #this function returns a list, so, self.token_set now is actually a list
     def load_token_list(self):
-        file_path = os.path.join(os.path.dirname(__file__),self.subfolder_name, f"{self.lang}_decoder_tokens_list.pkl")
+        file_path = os.path.join(self.subfolder_name, f"{self.lang}_decoder_tokens_list.pkl")
         with open(file_path, 'rb') as f:
             data = pickle.load(f)
             self.token_set = list(data['token_set'])
